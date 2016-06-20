fb009257397a9c1a367775994b9b4f5e47898a9b
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-06-19 22:33:11 -0400

    initial setup of functions

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// quote data
+var quotes = [
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  }
+];
+
+// returns a random quote
+function getRandomQuote() {
+  // select random quote from quotes array
+
+  // return the random quote
+}
+
+function getRandomColor() {
+    // returns random color from array
+}
+
+// prints quote to page when user clicks "Show Another Quote"
+function printQuote() {
+  // saves random quote in variable
+  var usedQuote = getRandomQuote();
+  var newColor = getRandomColor();
+}
